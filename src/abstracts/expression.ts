a7f009eb93e7227ba640df5c933e5f8e01cd45d3
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-10-14 20:51:12 +0530

    add typings for get, eval and task

--- src/abstracts/expression.ts
+++ src/abstracts/expression.ts
@@ -9,9 +9,10 @@ export class Expression<T_STATE = void> {
         computed(...dependencies)(this, methodName, descriptor);
     }
 
-    protected get<T>(name: keyof T_STATE, moduleName?: string): T | any;
-    protected get<T>(name: string, moduleName?: string): T | any;
-    protected get<T>(name: any, moduleName?: string): T | any {
+    protected get<T = any>(name: keyof T_STATE, moduleName?: string): T;
+    protected get<T = any>(name: string, moduleName?: string): T;
+    protected get<T = any>(name: any, moduleName?: string): T {
+        return null as T;
     }
 
 }
--- src/type.ts
+++ src/type.ts
@@ -1,17 +1,17 @@
-type GODAM_STATE1<T> = (name: keyof T, moduleName?: string) => any
-type GODAM_STATE2 = (name: string, moduleName?: string) => any
-type GODAM_STATE3 = (name: any, moduleName?: string) => any
+type GODAM_STATE1<T> = <T1 = any>(name: keyof T, moduleName?: string) => T1
+type GODAM_STATE2 = <T1 = any>(name: string, moduleName?: string) => T1
+type GODAM_STATE3 = <T1 = any>(name: any, moduleName?: string) => T1
 
 export type GODAM_STATE<T> = GODAM_STATE1<T> | GODAM_STATE2 | GODAM_STATE3;
 
-type GODAM_TASK1<T> = (taskName: keyof T, payload?: any) => any
-type GODAM_TASK2 = (taskName: string, payload?: any) => any
-type GODAM_TASK3 = (name: any, moduleName?: string) => any
+type GODAM_TASK1<T> = <T1 = any>(taskName: keyof T, payload?: any) => T1
+type GODAM_TASK2 = <T1 = any>(taskName: string, payload?: any) => T1
+type GODAM_TASK3 = <T1 = any>(name: any, moduleName?: string) => T1
 
 export type GODAM_TASK<T> = GODAM_TASK1<T> | GODAM_TASK2 | GODAM_TASK3;
 
-type GODAM_EVAL1<T> = (name: keyof T, payload?: any) => any
-type GODAM_EVAL2 = (name: string, payload?: any) => any
-type GODAM_EVAL3 = (name: any, payload?: any) => any
+type GODAM_EVAL1<T> = <T1 = any>(name: keyof T, payload?: any) => T1
+type GODAM_EVAL2 = <T1 = any>(name: string, payload?: any) => T1
+type GODAM_EVAL3 = <T1 = any>(name: any, payload?: any) => T1
 
 export type GODAM_EVAL<T> = GODAM_EVAL1<T> | GODAM_EVAL2 | GODAM_EVAL3;
